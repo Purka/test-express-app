e7958aee4a7a617b3573e9b7847764d0e0dda7f1
Gromova <malyarchyk.17@gmail.com>
2019-02-03 21:51:05 +0200

    Refactor event controller

--- event/eventController.js
+++ event/eventController.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const dateFormat = require('dateformat');
-const validateEvent = require('./helpers').validateEvent;
-const showOnlyMyEvents = require('./helpers').showOnlyMyEvents;
+const helpers = require('./helpers');
 const checkAuth = require('../auth/checkAuth');
 const Event = require('./Event');
 const User = require('../user/User');
@@ -13,16 +12,26 @@ router.use(bodyParser.json());
 
 router.post('/', (req, res) => {
     checkAuth(req, res, userId => {
-        const event = { title: req.body.title, text: req.body.text, date: req.body.date, id: userId };
+        const event = {
+            title: req.body.title,
+            text: req.body.text,
+            date: req.body.date,
+            id: userId
+        };
 
         try {
-            validateEvent(event);
+            helpers.validateEvent(event);
         } catch(e) {
             return res.status(400).send(e.message);
         }
 
         Event.create(event, (err, event) => {
-            if (err) return res.status(500).send('There was a problem adding the information to the database.');
+            if (err) {
+                return res.status(500).send({
+                    success: false,
+                    message: 'Fail to add the information to the database.'
+                });
+            }
             res.status(200).send(event.id);
         });
     });
@@ -31,8 +40,13 @@ router.post('/', (req, res) => {
 router.get('/', (req, res) => {
     checkAuth(req, res, userId => {
         Event.paginate({}, { page: 1, limit: 10 }, (err, events) => {
-            if (err) return res.status(500).send('There was a problem finding the events.');
-            let userEvents = showOnlyMyEvents(events.docs, userId);
+            if (err) {
+                return res.status(500).send({
+                    success: false,
+                    message: 'There was a problem finding the events.'
+                });
+            }
+            let userEvents = helpers.showOnlyMyEvents(events.docs, userId);
             //filter events by user's choice
             let filtered = userEvents.filter((event) => {
                 return dateFormat(event.date) === dateFormat(req.body.date);
@@ -45,26 +59,39 @@ router.get('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
     checkAuth(req, res, () => {
-        const event = { title: req.body.title, text: req.body.text, date: req.body.date };
+        const event = {
+            title: req.body.title,
+            text: req.body.text,
+            date: req.body.date
+        };
 
         try {
-            validateEvent(event);
+            helpers.validateEvent(event);
         } catch(e) {
             return res.status(400).send(e.message);
         }
 
         Event.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, event) => {
-            if (err) return res.status(500).send('There was a problem updating the event.');
+            if (err) {
+                return res.status(500).send({
+                    success: false,
+                    message: 'There was a problem updating the event.'
+                });
+            }
             res.status(200).send(event);
         });
     });
 });
 
 router.delete('/:id', (req, res) => {
-    console.log(req.params.id);
     checkAuth(req, res, () => {
         Event.findByIdAndRemove(req.params.id, (err, event) => {
-            if (err) return res.status(500).send('There was a problem deleting the event.');
+            if (err) {
+                return res.status(500).send({
+                    success: false,
+                    message: 'There was a problem deleting the event.'
+                });
+            }
             res.status(200).send('Event '+ event.title +' was deleted.');
         });
     });
